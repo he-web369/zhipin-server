49aec77a8a4b2d09cddf6ad5dedfe4a07825cc8a
he-web <897940731@qq.com>
2019-12-07 16:04:36 +0800

    加入scoket.io

--- db/modules.js
+++ db/modules.js
@@ -4,7 +4,7 @@ const mongoose=require('mongoose')
 mongoose.connect('mongodb://localhost:27017/zhipin',{ useNewUrlParser: true,useUnifiedTopology: true })
 const conn=mongoose.connection
 conn.on('connected',function () {
-    console.log('linking success')
+    console.log('database-linking success')
 })
 const userSchema=mongoose.Schema({
     username:{type:String,required:true},
@@ -16,5 +16,15 @@ const userSchema=mongoose.Schema({
     company:{type:String},//公司
     salary:{type:String}//工资
 })
+const chatSchema=mongoose.Schema({
+    from:{type:String,required:true},//发送用户的id
+    to:{type:String,required:true},//接收用户的id
+    chat_id:{type:String,required:true},//from和to 组成的字符串
+    content:{type:String,required:true},//内容
+    read:{type:Boolean,default:false},//是否已读
+    create_time:{type:String}//创建时间
+})
 const UserModel = mongoose.model('user',userSchema)
+const ChatModel=mongoose.model('chat',chatSchema)
 exports.UserModel=UserModel
+exports.ChatModel=ChatModel
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const {UserModel}=require('../db/modules')
+const {UserModel,ChatModel}=require('../db/modules')
 const md5=require('blueimp-md5')
 const filter={password:0,__v:0}
 
@@ -78,11 +78,38 @@ router.get("/user",function (req,res) {
     if(!userId){
         return res.send({code:1,msg:'请先登录'})
     }
-    UserModel.findOne({_id:userId},function (err,user) {
+    UserModel.findOne({_id:userId},filter,function (err,user) {
         if(user){
             res.send({code:0,data:user})
         }
     })
 })
-
+//获取用户列表（根据用户类型）
+router.get('/userlist',function (req,res) {
+    const {type}=req.query
+    UserModel.find({type},filter,function (err,users) {
+        res.send({code:0,data:users})
+    })
+})
+//获取当前用户的聊天消息列表
+router.get('/msglist',function (req,res) {
+    const {userid}=req.cookies
+    UserModel.find(function (err,users) {
+        const usersContainer={}
+        users.forEach(user=>{
+            usersContainer[user._id]={username:user.username,header:user.header}
+        })
+        ChatModel.find({'$or':[{from:userid},{to:userid}]},filter,function (err,chatMsgs) {
+            res.send({code:0,data:{usersContainer,chatMsgs}})
+        })
+    })
+})
+//修改指定消息为已读
+router.post('/readmsg',function (req,res) {
+    const from=req.body.from
+    const to=req.cookies.userid
+    ChatModel.update({from,to,read:false},{read: true},{multi:true},function (err,chat) {
+        res.send({code:0,data:chat.nModified})
+    })
+})
 module.exports = router;
